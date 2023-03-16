535de8de20ca5388b7e7d545a567586b5ff6a4d7
Gabrielt <gabrielmarques@compels.net>
2023-03-15 21:15:06 -0300

    Pagge-07 Cadastro de Livros Banco - Solução Salvar livro

--- src/main/java/com/senai/pagge/controller/LivroController.java
+++ src/main/java/com/senai/pagge/controller/LivroController.java
@@ -1,10 +1,16 @@
 package com.senai.pagge.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.senai.pagge.entities.Livro;
 import com.senai.pagge.services.LivroService;
 
@@ -14,8 +20,10 @@ public class LivroController {
     @Autowired
     private LivroService livroService;
 
-    @PostMapping(value = "/save")
-    public void save(Livro livro) {
+    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void save(@RequestBody String jsonData) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Livro livro = objectMapper.readValue(jsonData, Livro.class);
         livroService.save(livro);
     }
 }
--- src/main/resources/static/services/livros/livroService.js
+++ src/main/resources/static/services/livros/livroService.js
@@ -11,12 +11,48 @@ if (telaAtual == "/livros") {
     function btnAdicionarClick() {
         window.open('/cadastroLivro', '_self');
     }
-    
+
     //verica a tela que o usuario está vendo
-}else if (telaAtual == "/cadastroLivro") { 
+} else if (telaAtual == "/cadastroLivro") {
     document.querySelector('.bntFechar').addEventListener('click', btnFecharClick);
+    document.querySelector('.bntSalvar').addEventListener('click', salvarLivro);
 
     function btnFecharClick(evento) {
         window.open('/livros', '_self');
+    };
+
+    function salvarLivro(evento) {
+        evento.preventDefault();
+
+        const livro = {
+            "id": document.querySelector("#id").value,
+            "titulo": document.querySelector("#titulo").value,
+            "autor": document.querySelector("#autor").value,
+            "editora": document.querySelector("#editora").value,
+            "paginas": document.querySelector("#pagina").value,
+            "genero": document.querySelector("#genero").value,
+            "isbn": document.querySelector("#isbn").value,
+            "status": document.querySelector("#status").value,
+            "observacao": document.querySelector("#observacao").value
+        };
+
+        var requisicao = new XMLHttpRequest();
+        
+        requisicao.open("post", "/livro/save");
+        requisicao.setRequestHeader('Content-Type', 'application/json');
+        
+        requisicao.send(JSON.stringify(livro).toString());
+
+        requisicao.onreadystatechange = function () {
+            if (requisicao.readyState == 4) {
+                if (requisicao.status == 200) {
+                    window.open('/livros', '_self');
+                } else {
+                    alert("Erro ao salvar o livro. Por favor, entre em contato com o suporte.")
+                }
+            }
+        }
+
+
     }
 }
