51c79e9f236e3818c022e4037b1cb807cdfdee21
Gabrielt <gabrielmarques@compels.net>
2023-03-16 21:59:03 -0300

    Pagge-07 Cadastro de Livros Banco - Método de Editar - Método de Salvar

--- src/main/java/com/senai/pagge/controller/LivroController.java
+++ src/main/java/com/senai/pagge/controller/LivroController.java
@@ -2,12 +2,12 @@ package com.senai.pagge.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -32,5 +32,10 @@ public class LivroController {
     @GetMapping(value = "/findById/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Livro findById(@PathVariable Long id){
         return livroService.findById(id);
+    }
+
+    @DeleteMapping(value = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable Long id){
+        livroService.delete(id);
     }
 }
--- src/main/resources/static/services/livros/livroService.js
+++ src/main/resources/static/services/livros/livroService.js
@@ -3,53 +3,65 @@ import { getApi } from "../utils/Api.js";
 
 var telaAtual = window.location.pathname;
 
+//verica a tela que o usuario está vendo
 if (telaAtual.includes("/livros")) {
     window.onload = function () {
         renderMenuLateral();
         var tabelaLivro = document.querySelector('.tabela');
 
+        //Local onde se adiciona os eventos da tela
         document.querySelector('.btnAdicionar').addEventListener('click', btnAdicionarClick);
-        for(let i = 0; i <tabelaLivro.rows.length-1; i++){
-            var linha = tabelaLivro.rows[i+1];
+        for (let i = 0; i < tabelaLivro.rows.length - 1; i++) {
+            var linha = tabelaLivro.rows[i + 1];
             linha.addEventListener('dblclick', editarLivro);
-        }
+            linha.querySelector('.deleteColuna').addEventListener('click', deletarLivro);
+        }//Adiciona evento a cada linha da tabela
     }
 
     function btnAdicionarClick() {
-        window.open('/cadastroLivro/""', '_self');
+        window.open('/cadastroLivro/novo', '_self');
     }
 
-    function editarLivro(evento){
+    function editarLivro(evento) {
         var linhaSelecionada = evento.currentTarget;
         var codigo = linhaSelecionada.querySelector('.codigoColuna').innerText;
+        window.open('/cadastroLivro/' + codigo, '_self');
+    }
 
-        window.open('/cadastroLivro/'+codigo, '_self');
-        var requisicao = new XMLHttpRequest();
-        
-        requisicao.open("get", "/livro/findById/"+codigo);
-        requisicao.setRequestHeader('Content-Type', 'application/json');
-        
-        requisicao.send(codigo);
+    function deletarLivro(evento){
+        var linhaSelecionada = evento.currentTarget.parentElement;
+        var codigo = linhaSelecionada.querySelector('.codigoColuna').innerText;
 
-        requisicao.onreadystatechange = function () {
-            if (requisicao.readyState == 4) {
-                if (requisicao.status == 200) {
-                } else {
-                    alert("Erro ao salvar o livro. Por favor, entre em contato com o suporte.")
+        if(confirm("Deseja excluir o livro "+codigo+"? ")){
+            var requisicao = new XMLHttpRequest();
+            requisicao.open("delete", "/livro/delete/" + codigo);
+            requisicao.setRequestHeader('Content-Type', 'application/json');
+            requisicao.send();
+    
+            requisicao.onreadystatechange = function () {
+                if (requisicao.readyState == 4) {
+                    if (requisicao.status == 200) {
+                        window.open('/livros', '_self');
+                    }
                 }
             }
         }
 
+
     }
 
-    //verica a tela que o usuario está vendo
 } else if (telaAtual.includes("/cadastroLivro")) {
 
     window.onload = function (win) {
+
+        //Verifica se a url do Cadastro de Livros possui ID ou se é um novo cadastro
         var api = getApi(win);
-        if(api.parametros != "" && api.parametros != null){
+        if (api.parametros != "novo") {
+            //Caso tenha ID, vai chamar o método abaixo passando o ID como parametro
             carregarFormulario(api.parametros);
         }
+
+        //Local onde se adiciona os eventos da tela
         document.querySelector('.bntFechar').addEventListener('click', btnFecharClick);
         document.querySelector('.bntSalvar').addEventListener('click', salvarLivro);
     }
@@ -58,8 +70,33 @@ if (telaAtual.includes("/livros")) {
         window.open('/livros', '_self');
     };
 
-    function carregarFormulario(){
+    function carregarFormulario(livroId) {
 
+        //Faz um findById no banco para buscar os dados do livro
+        var requisicao = new XMLHttpRequest();
+        requisicao.open("get", "/livro/findById/" + livroId);
+        requisicao.setRequestHeader('Content-Type', 'application/json');
+        requisicao.send();
+
+        requisicao.onreadystatechange = function () {
+            if (requisicao.readyState == 4) {
+                if (requisicao.status == 200) {
+                    //Dados dos Livro vindos do banco de dados
+                    var livro = JSON.parse(requisicao.responseText);
+
+                    //Preenche o formulário com os dados do livro
+                    document.querySelector("#id").value = livro.id;
+                    document.querySelector("#titulo").value = livro.titulo;
+                    document.querySelector("#autor").value = livro.autor;
+                    document.querySelector("#editora").value = livro.editora;
+                    document.querySelector("#pagina").value = livro.paginas;
+                    document.querySelector("#genero").value = livro.genero;
+                    document.querySelector("#isbn").value = livro.isbn;
+                    document.querySelector("#status").value = livro.status;
+                    document.querySelector("#observacao").value = livro.observacao;
+                }
+            }
+        }
     }
 
     function salvarLivro(evento) {
@@ -78,10 +115,10 @@ if (telaAtual.includes("/livros")) {
         };
 
         var requisicao = new XMLHttpRequest();
-        
+
         requisicao.open("post", "/livro/save");
         requisicao.setRequestHeader('Content-Type', 'application/json');
-        
+
         requisicao.send(JSON.stringify(livro).toString());
 
         requisicao.onreadystatechange = function () {
@@ -93,7 +130,6 @@ if (telaAtual.includes("/livros")) {
                 }
             }
         }
-
 
     }
 }
